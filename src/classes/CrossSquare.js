760f9c83823c168a4da4b8e173c8d75622f40782
baedi <dlbae9613@gmail.com>
2023-06-20 23:58:30 +0900

    Add RenjuRule and Token Class

--- src/classes/CrossSquare.js
+++ src/classes/CrossSquare.js
@@ -1,7 +1,9 @@
+import Token from 'classes/Token'
+
 function CrossSquare(props){
     return(
         <button className='cross_square' onClick={props.onClick}>
-            {props.value}
+            {props.value === Token.black() ? '●' : props.value === Token.white() ? '○' : ''}
         </button>
     );
 }
--- src/classes/GameBoard.js
+++ src/classes/GameBoard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import CrossSquare from 'classes/CrossSquare'
+import Token from 'classes/Token'
+import RenjuRule from "./RenjuRule";
 
 class GameBoard extends React.Component{
     constructor(props) {
@@ -8,18 +10,18 @@ class GameBoard extends React.Component{
         let arr = Array.from({length: 15}, () => []);
         for(let i=0; i<arr.length; i++){
             for(let j=0; j<arr.length; j++){
-                arr[i].push('')
+                arr[i].push(Token.empty())
             }
         }
 
         this.state = {
-            //crossSquares: Array(15).fill(Array(15).fill(null)),
             crossSquares: arr,
             isBlackTurn: true,
             isEndGame: false
         }
     }
     handleClick(row, col){
+        let maxConnTokenCnt;
         const crossSquares = this.state.crossSquares.slice();
         let isBlackTurn = this.state.isBlackTurn;
         let isEndGame = this.state.isEndGame;
@@ -28,24 +30,32 @@ class GameBoard extends React.Component{
             return;
 
         // 이미 말이 놓여있는지 확인
-        if(crossSquares[row][col] !== ''){
+        if(crossSquares[row][col] !== Token.empty()){
             alert("This Square has already token.")
             return;
         }
 
-        // 렌주룰 적용 코드
-        if(isBlackTurn && this.ruleCheck(row, col)){
-            alert("")
-            return;
-        }
-
-        crossSquares[row][col] = isBlackTurn ? '●' : '○';
-
         // 말 5개 완성 여부
-        if(this.hasMakeFiveToken(isBlackTurn, crossSquares, row, col)){
+        maxConnTokenCnt = RenjuRule.maxConnectToken(isBlackTurn, crossSquares, row, col)
+        if(maxConnTokenCnt >= 5){
             alert((isBlackTurn ? "Black" : "White") + " Win!");
             isEndGame = true;
         }
+
+        // 렌주룰 적용 코드
+        if(isBlackTurn){
+            if(RenjuRule.check33(crossSquares, row, col)){
+                alert("");
+                return;
+            }
+
+            if(RenjuRule.check44(crossSquares, row, col)){
+                alert("");
+                return;
+            }
+        }
+
+        crossSquares[row][col] = isBlackTurn ? Token.black() : Token.white();
 
         this.setState({
             crossSquares: crossSquares,
@@ -53,39 +63,6 @@ class GameBoard extends React.Component{
             isEndGame: isEndGame,
         });
     }
-
-    ruleCheck(row, col){
-        return false;
-    }
-
-    hasMakeFiveToken(isBlackTurn, crossSquares, row, col){
-
-        let tokenType = isBlackTurn ? '●' : '○';
-        let plusX, plusY;
-        let maxLen = this.state.crossSquares.length;
-        let token = [1, 0, 1, 0, 1, 0, 1, 0];
-        let moveX = [1, -1, 0,  0, 1, -1,  1, -1];
-        let moveY = [0,  0, 1, -1, 1, -1, -1,  1];
-
-        for(let i=0; i<token.length; i++){
-            plusX = moveX[i];
-            plusY = moveY[i];
-            for(let r=row+plusX, c=col+plusY; 0 <= r && r < maxLen && 0 <= c && c < maxLen; r = r + plusX, c = c + plusY){
-                if(crossSquares[r][c] === tokenType)
-                    token[i]++;
-                else
-                    break;
-            }
-
-            if(i % 2 === 1 && token[i-1] + token[i] >= 5)
-                return true;
-
-        }
-        //console.log(token)
-
-        return false;
-    }
-
 
     makeColGameBoard(colArr, row){
         const handleObj = this;
